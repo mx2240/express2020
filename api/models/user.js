eb339d5862719656cfc5748cb793c7adf72369e3
mx2240 <lovemicky10@gmail.com>
2025-11-20 14:21:51 +0000

    up

--- api/models/user.js
+++ api/models/user.js
@@ -2,21 +2,13 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: String,
+    email: String,
+    password: String,
     role: { type: String, enum: ["admin", "student", "parent"], default: "student" },
     // isVerified: { type: Boolean, default: false },        // email verification
-}, { timestamps: true });
+},);
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
 
-userSchema.methods.matchPassword = async function (entered) {
-    return await bcrypt.compare(entered, this.password);
-};
 
 module.exports = mongoose.model("User", userSchema);
