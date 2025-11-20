2e7db91d3091a22b9d2af9f41f0c509f7ec1d56a
mx2240 <lovemicky10@gmail.com>
2025-11-20 22:44:01 +0000

    up

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const app = express();
 // ===========================
 app.use(cors({
 
-    origin: "http://localhost:3000",
+    origin: "https://my-frontend-brown-eta.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 
