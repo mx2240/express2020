129a0cd408cfcd13693dbec0e090e437bd284b32
mx2240 <lovemicky10@gmail.com>
2025-11-20 19:04:39 +0000

    up

--- api/app.js
+++ api/app.js
@@ -99,4 +99,7 @@ app.get("/", (req, res) => {
 // Start server
 // ===========================
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+
+
+module.exports = app;
--- config/db.js
+++ config/db.js
@@ -2,14 +2,31 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
-        process.exit(1);
+    if (cached.conn) {
+        return cached.conn; // reuse existing connection
     }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+            // add other mongoose options if needed
+        };
+
+        cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+            console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+            return mongoose;
+        });
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
 };
 
 module.exports = connectDB;
