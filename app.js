7ae654573418ab515844cdf0d6789046442366e3
mx2240 <lovemicky10@gmail.com>
2025-11-23 10:03:01 +0000

    up

--- app.js
+++ app.js
@@ -130,8 +130,8 @@ app.get("/", (req, res) => {
 // ===========================
 // Start server
 // ===========================
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
 
 
 // Global Error Handler
