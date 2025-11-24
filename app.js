5bbcfe63851c23eb86271aa095c6d24792bb1ffa
mx2240 <lovemicky10@gmail.com>
2025-11-24 04:23:46 +0000

    up

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.get("/", (req, res) => {
 // ===========================
 // Start server
 // ===========================
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
 
 
