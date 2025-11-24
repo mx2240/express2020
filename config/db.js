a3cdc4fda2fc1c710bd5a855dcccd82970e3aeb3
mx2240 <lovemicky10@gmail.com>
2025-11-24 03:43:25 +0000

    up

--- config/db.js
+++ config/db.js
@@ -1,46 +1,13 @@
-// // config/db.js
-// const mongoose = require("mongoose");
-
-// let cached = global.mongoose;
-
-// if (!cached) {
-//     cached = global.mongoose = { conn: null, promise: null };
-// }
-
-// async function connectDB() {
-//     if (cached.conn) return cached.conn;
-
-//     if (!cached.promise) {
-//         const opts = {
-//             bufferCommands: false,
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         };
-//         cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
-//             return mongoose;
-//         });
-//     }
-
-//     cached.conn = await cached.promise;
-//     return cached.conn;
-// }
-
-// module.exports = connectDB;
-
-
-
-
+// db.js
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI); // no options needed
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
-        process.exit(1);
-    }
+    if (isConnected) return;
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("MongoDB connected");
 };
 
 module.exports = connectDB;
-
