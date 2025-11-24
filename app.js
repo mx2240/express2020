3f2d3de758a363971c65e92dad975775a3c85c82
mx2240 <lovemicky10@gmail.com>
2025-11-24 05:34:47 +0000

    up

--- app.js
+++ app.js
@@ -87,7 +87,8 @@ app.get("/", (req, res) => {
 module.exports.handler = serverless(app);
 
 // Optional: local dev server
-if (require.main === module) {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
-}
+// if (require.main === module) {
+//     const PORT = process.env.PORT || 5000;
+//     app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+// }
+// // 
